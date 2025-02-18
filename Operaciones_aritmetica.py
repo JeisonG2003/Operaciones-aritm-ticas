ddb8bcda14183e5baf3217b2aeccaa1cd7c8e5be
JeisonG2003 <jt.garciaa@uea.edu.ec>
2025-02-18 12:31:43 -0500

    Add files via upload

---
+++ Operaciones_aritmetica.py
@@ -0,0 +1,13 @@
+# Trabajo De Programación De Jeison Garcia
+
+# Operadores aritméticos
+# dos números para realizar operaciones matemáticas
+numero1 = 25
+numero2 = 4
+# Operaciones aritméticas
+suma = numero1 + numero2  # Suma de dos números
+multiplicacion = numero1 * numero2  # Multiplicación de dos números
+
+print("\n--- Operadores Aritméticos ---")
+print(f"La suma de {numero1} y {numero2} es: {suma}")
+print(f"La multiplicación de {numero1} y {numero2} es: {multiplicacion}")
